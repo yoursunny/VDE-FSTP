68ea92c7d4f9b2af5a0321ccf7403fb6bcbebc41
Teng Liang <philoliang2011@gmail.com>
2016-04-07 12:09:52 -0700

    add root path function

--- vde_runner.py
+++ vde_runner.py
@@ -1,11 +1,33 @@
 import subprocess
 import sys
+import time
+
+class Node(object):
+    def __init__(self, name):
+        self.name = name
+        self.nPorts = 0
+        self.portNode = {}
+
+    def addLink(self, node):
+        self.nPorts += 1
+        self.portNode[self.nPorts] = node
+
+    def getPort(self, node):
+        for i in range(1, self.nPorts+1):
+            if self.portNode[i] == node:
+                return i
+
+    def __str__(self):
+        return self.name
 
 class VDE_Runner(object):
     def __init__(self, configureFileName):
         self.nodes = []
         self.links = []
-        self.rootNode = ""
+        self.rootNode = object()
+
+        #Node,Port
+        self.rootPaths = []
 
         #construct the topology via conf file     
         configureFile = open(configureFileName,"r")
@@ -25,61 +47,92 @@ class VDE_Runner(object):
 
             if isReadingNodes:
                 eachLineList = eachLine.split(":")
-                self.nodes.append(eachLineList[0])
+                newNode = Node(eachLineList[0])
+                self.nodes.append(newNode)
                 if "root" in eachLineList[1]:
-                    self.rootNode = eachLineList[0]
+                    self.rootNode = newNode
 
             if isReadingLinks:
-                #eachLineList = eachLine.split[":"]
+                
                 #sourceNode = eachLineList[0].split["-"][0]
                 #destinationNode = eachLineList[0].split["-"][1]
                 eachLine = eachLine[:-1]
-                s = eachLine.split("-")[0]
-                d = eachLine.split("-")[1]
-                self.links.append((s,d))
+                eachLineList = eachLine.split(":")
+
+                firtPart = eachLineList[0]
+                sourceNode = self.findNode(firtPart.split("-")[0])
+                destNode = self.findNode(firtPart.split("-")[1])
+                self.links.append((sourceNode,destNode))
+
+                sourceNode.addLink(destNode)
+                destNode.addLink(sourceNode)
+
+                if "root" in eachLineList[1]:
+                    self.rootPaths.append((sourceNode,destNode))
 
         print self.nodes
         print self.links
+        print self.rootPaths
+
+    def findNode(self,name):
+        for eachNode in self.nodes:
+            if eachNode.name == name:
+                return eachNode
 
     def runVDE(self):
     	print "-----------------------Start to setup all switchs with links-------------------------"
         subprocess.call(["killall","vde_switch"])
         subprocess.call(["killall","vde_plug"])
 
+        #add nodes in VDE
         for eachNode in self.nodes:
-            
 
             if eachNode == self.rootNode:
-                subprocess.call(["vde_switch","-d","-s","/tmp/switch-"+eachNode, "-M", "/tmp/mgmt-"+eachNode, "--macaddr", "00:00:00:00:00:01"])
+                subprocess.call(["vde_switch","-d","-s","/tmp/switch-"+eachNode.name, "-M", "/tmp/mgmt-"+eachNode.name, "--macaddr", "00:00:00:00:00:01"])
             else:
-                subprocess.call(["vde_switch","-d","-s","/tmp/switch-"+eachNode, "-M", "/tmp/mgmt-"+eachNode])
+                subprocess.call(["vde_switch","-d","-s","/tmp/switch-"+eachNode.name, "-M", "/tmp/mgmt-"+eachNode.name])
 
-            subprocess.call(["mkfifo","/tmp/fifo-"+eachNode])
+            subprocess.call(["mkfifo","/tmp/fifo-"+eachNode.name])
 
-            subprocess.call("echo 'plugin/add /usr/local/lib/vde2/plugins/pdump.so' |nc -U /tmp/mgmt-"+eachNode, shell=True)
-            command_dump = "echo 'pdump/filename /tmp/myfifo-"+eachNode+"' | nc -U /tmp/mgmt-"+eachNode
+            subprocess.call("echo 'plugin/add /usr/local/lib/vde2/plugins/pdump.so' |nc -U /tmp/mgmt-"+eachNode.name, shell=True)
+            command_dump = "echo 'pdump/filename /tmp/myfifo-"+eachNode.name+"' | nc -U /tmp/mgmt-"+eachNode.name
             subprocess.call(command_dump, shell=True)
-            subprocess.call("echo 'pdump/buffered 0' |nc -U /tmp/mgmt-"+eachNode, shell=True)
-            subprocess.call("echo 'pdump/active 1' |nc -U /tmp/mgmt-"+eachNode, shell=True)
+            subprocess.call("echo 'pdump/buffered 0' |nc -U /tmp/mgmt-"+eachNode.name, shell=True)
+            subprocess.call("echo 'pdump/active 1' |nc -U /tmp/mgmt-"+eachNode.name, shell=True)
 
+        #add links in VDE
         for eachLink in self.links:
-            s = eachLink[0]
-            d = eachLink[1]
+            sourceNode = eachLink[0]
+            destNode = eachLink[1]
 
-            command = "dpipe -d vde_plug /tmp/switch-"+s+" = vde_plug /tmp/switch-"+d
+            command = "dpipe -d vde_plug /tmp/switch-"+sourceNode.name+" = vde_plug /tmp/switch-"+destNode.name
             subprocess.call(command, shell=True)
+
+        #add bonus ports in VDE
+        print "-----------------------Start to setup bonus ports-------------------------"
+        for eachLink in self.rootPaths:
+            sourceNode = eachLink[0]
+            destNode = eachLink[1]
+
+            #!!destNode is the node that has multple choices
+            destPort = destNode.getPort(sourceNode)
+
+            command = "echo 'fstp/bonus 0 "+str(destPort)+" 10000000' |nc -U /tmp/mgmt-"+destNode.name
+            print command
+            subprocess.call(command, shell=True)
+
 
     def printFSTP(self):
     	print "-----------------------FSTP/PRINT for all switchs-------------------------"
         for eachNode in self.nodes:
-            command = "echo 'fstp/print' | nc -U  /tmp/mgmt-"+eachNode
+            command = "echo 'fstp/print' | nc -U  /tmp/mgmt-"+eachNode.name
             subprocess.call(command, shell=True)
 
 
     def enableFSTP(self):
     	print "-----------------------Enable FSTP for all switchs-------------------------"
     	for eachNode in self.nodes:
-            command = "echo 'fstp/setfstp 1' | nc -U  /tmp/mgmt-"+eachNode
+            command = "echo 'fstp/setfstp 1' | nc -U  /tmp/mgmt-"+eachNode.name
             subprocess.call(command, shell=True)
 
 if __name__ == "__main__":
@@ -90,3 +143,5 @@ if __name__ == "__main__":
         vde_runner.runVDE()
         vde_runner.printFSTP()
         vde_runner.enableFSTP()
+        time.sleep(5)
+        vde_runner.printFSTP()
